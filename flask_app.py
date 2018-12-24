fb8803c7314b985fa12ede836813a2fdf42b1118
clemnjord <clementpuech@protonmail.com>
2018-12-24 18:51:05 +0100

    Adding request to api

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,14 @@
-
-# A very simple Flask Hello World app for you to get started with...
-
+import http.client
+import json
 from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello from Flask!'
-
+    connection = http.client.HTTPConnection('api.football-data.org')
+    headers = { 'X-Auth-Token': 'fe4c5aaa344a40a78cef8547f5840478' }
+    connection.request('GET', '/v2/matches', None, headers )
+    response = json.loads(connection.getresponse().read().decode())
+    
+    return 'Hello from Flask!' + '\n' + response
